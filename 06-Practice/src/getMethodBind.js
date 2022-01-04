8609e325d93f7bc8f7056a572fecc9f71a4360c5
Olha Starosta <shop2029@gmail.com>
2022-01-04 10:30:10 +0200

    getMethodBind

--- 06-Practice/src/getMethodBind.js
+++ 06-Practice/src/getMethodBind.js
@@ -1,5 +1,19 @@
 // 5) Напишите свою реализацию метода bind
 
+const person = {
+    name: 'Olha'
+}
 
+function info() {
+   console.log(`Имя: ${this.name}`) ;
+}
 
-module.exports = { getMethodBind };
+function getMethodBind(fn, context) {
+    return function () {
+        return fn.apply(context)
+    }
+}
+
+getMethodBind(info, person)()
+
+module.exports = {getMethodBind};
